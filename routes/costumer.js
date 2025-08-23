fb9531701bb59140d4d04a9bd23c187849137269
Rhyan <rhyan@gmail.com>
2025-08-23 09:24:35 -0300

    CRUD Operations

--- routes/costumer.js
+++ routes/costumer.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express.Router();
 
 app.get("/", (req, res) => {
-    const select = "select * from costumer";
+    const select = "SELECT * from costumer";
     bd.query(select, function(err, results){
         if(err){
             console.log(err);
@@ -12,5 +12,56 @@ app.get("/", (req, res) => {
         }
     });
 });
+
+app.get("/:id", (req,res)=>{
+    const select = "SELECT * from costumer where id_cliente = ? ";
+    bd.query(select, [req.params.id], function(err,results){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(results);
+        }
+    });
+});
+
+app.post("/insert", (req, res) => {
+    const insert = "INSERT INTO costumer SET CPF_CNPJ = ?, nome = ?";
+    const body = req.body;
+    bd.query(insert, [body.CPF_CNPJ,body.nome], function(err, results){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Novo Cliente Inserido ao BD!");
+            res.send(results);
+        }
+    });
+});
+
+app.put("/insert/:id", (req, res) => {
+    const insert = "UPDATE costumer SET CPF_CNPJ = ?, nome = ? WHERE id_cliente=?";
+    const body = req.body;
+    bd.query(insert, [body.CPF_CNPJ,body.nome,req.params.id], function(err, results){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Cliente atualizado com sucesso!");
+            res.send(results);
+        }
+    });
+});
+
+app.delete("/del/:id", (req,res)=>{
+    const del = "DELETE FROM costumer WHERE id_cliente = ?";
+    bd.query(del, [req.params.id], function(err, results){
+        if(err){
+            console.log(err);
+        }else{
+            res.send("Cliente deletado!");
+            
+        }
+    });
+    
+});
+
 
 module.exports = app;
--- routes/doctor.js
+++ routes/doctor.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express.Router();
 
 app.get("/", (req, res) => {
-    const select = "select * from doctor";
+    const select = "SELECT * from doctor";
     bd.query(select, function(err, results){
         if(err){
             console.log(err);
@@ -11,8 +11,58 @@ app.get("/", (req, res) => {
             res.send(results);
         }
     });
-
 });
+
+app.get("/:id", (req,res)=>{
+    const select = "SELECT * from doctor where id_medico = ? ";
+    bd.query(select, [req.params.id], function(err,results){
+        if(err){
+            console.log(err);
+        }else{
+            res.send(results);
+        }
+    });
+});
+
+app.post("/insert", (req, res) => {
+    const insert = "INSERT INTO doctor SET licenca = ?, nome = ?";
+    const body = req.body;
+    bd.query(insert, [body.licenca,body.nome], function(err, results){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Novo Cliente Inserido ao BD!");
+            res.send(results);
+        }
+    });
+});
+
+app.put("/insert/:id", (req, res) => {
+    const insert = "UPDATE doctor SET licenca = ?, nome = ? WHERE id_medico=?";
+    const body = req.body;
+    bd.query(insert, [body.licenca,body.nome,req.params.id], function(err, results){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Médico atualizado com sucesso!");
+            res.send(results);
+        }
+    });
+});
+
+app.delete("/del/:id", (req,res)=>{
+    const del = "DELETE FROM doctor WHERE id_medico = ?";
+    bd.query(del, [req.params.id], function(err, results){
+        if(err){
+            console.log(err);
+        }else{
+            res.send("Doutor deletado!");
+            
+        }
+    });
+    
+});
+
 
 module.exports = app;
 
