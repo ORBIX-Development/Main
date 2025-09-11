ab9cae9878942b86b5046a7709d559d2059e78ea
RhyanO2 <rhyan123@email.com>
2025-09-11 07:55:17 -0300

    Fix (Local Machine Run)

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
-const mysql = require("mysql2")
+const mysql = require("mysql2");
+const senha = require("./senha");
 const connection = mysql.createConnection(
     {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: process.env.DB_HOST || "localhost",
+        user: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || senha(),
+        database: process.env.DB_NAME || "obx",
     }
 );
 
