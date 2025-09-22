59a8bdb72f009ffff6e026fac9c8f480165c3869
RhyanO2 <rhyanoliveirads@gmail.com>
2025-09-22 20:41:45 -0300

    hashPassword add

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,5 +1,6 @@
 const bd = require('../connection');
 const express = require('express');
+const bcrypt = require('bcrypt')
 const app = express.Router();
 
 app.get("/", (req, res) => {
@@ -24,30 +25,49 @@ app.get("/:id", (req,res)=>{
     });
 });
 
-app.post("/insert", (req, res) => {
-    const insert = "INSERT INTO usuario SET nome = ? ,email = ?, senha = ?,cargo = ?, cod_doc = ?";
-    const body = req.body;
-    bd.query(insert, [body.nome,body.email,body.senha,body.cargo,body.cod_doc], function(err, results){
+app.post("/insert", async (req, res) => {
+    try{ 
+        const body = req.body;
+        const hashPassword = await bcrypt.hash(body.senha, 10);
+        const insert = "INSERT INTO usuario SET nome = ? ,email = ?, senha = ?,cargo = ?, cod_doc = ?";
+
+        bd.query(insert, [body.nome,body.email,hashPassword,body.cargo,body.cod_doc],
+             function(err, results){
         if(err){
             console.log(err);
+            res.status(500).send("Erro ao inserir usuário");
         }else{
             console.log("Novo Usuário Inserido!");
             res.send("Novo Usuário Inserido!");
         }
     });
+       } catch (err){
+        console.error(err);
+        res.status(500).send("Erro ao processar a requisição");
+    }
 });
 
-app.put("/insert/:id", (req, res) => {
-    const update = "UPDATE usuario SET nome = ? ,email = ?, senha = ?, cod_doc = ? WHERE id=?";
-    const body = req.body;
-    bd.query(update, [body.nome,body.email,body.senha,body.cod_doc,req.params.id], function(err, results){
+app.put("/insert/:id", async (req, res) => {
+    try{
+        const body = req.body;
+        let hashPassword = null;
+        if(body.senha) {
+            hashPassword = await bcrypt.hash(body.senha, 10);
+        }
+        const update = "UPDATE usuario SET nome = ? ,email = ?, senha = ?,cargo = ?, cod_doc = ? WHERE id=?";
+        bd.query(update, [body.nome,body.email,hashPassword,body.cargo,body.cod_doc,req.params.id], function(err, results){
         if(err){
             console.log(err);
+            res.status(500).send("Erro ao atualizar usuário")
         }else{
-            console.log("Cliente atualizado!");
+            console.log("Usuário atualizado!");
             res.send(results);
         }
     });
+    }catch (err){
+        console.error(err);
+        res.status(500).send("Erro ao processar a requisição");
+    }
 });
 
 app.delete("/del/:id", (req,res)=>{
